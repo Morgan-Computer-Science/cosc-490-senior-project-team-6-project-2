13b9f49aeb2efab83d3676631ca8a7b1596ff1a6
arcor3 <arcor3@morgan.edu>
2026-03-12 11:53:50 -0400

    Refactor main.py to streamline agent execution
    Refactored main.py to use run_agents function and removed unused imports and code.

--- main.py
+++ main.py
@@ -1,56 +1,17 @@
-from dotenv import load_dotenv
-from pydantic import BaseModel 
-from langchain_openai import ChatOpenAI
-from langchain_anthropic import ChatAnthropic
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import PydanticOutputParser
-from langchain_classic.agents import create_tool_calling_agent, AgentExecutor
-from tools import search_tool, wiki_tool, save_tool
+from pathlib import Path
+from coordinator.run_agents import run_agents
 
-load_dotenv()
+def main():
 
-class ResearchResponse(BaseModel):
-    topic: str
-    summary: str
-    sources: list[str]
-    tools_used: list[str]
+    scenario = Path("scenarios/recession_scenario.txt").read_text()
 
+    results = run_agents(scenario)
 
-llm = ChatAnthropic(model="claude-sonnet-4-5-20250929")
-parser = PydanticOutputParser(pydantic_object=ResearchResponse)
+    for agent, output in results.items():
+        print("\n========================")
+        print(agent)
+        print("========================")
+        print(output)
 
-prompt = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            """ 
-            You are a helpful assistant that can answer questions and help with tasks.
-            Answer the question based on the information provided and use the necessary tools.
-            Wrap the output in the format and proide no other text\n{format_instructions}
-            """,
-        ),
-        ("placeholder", "{chat_history}"),
-        ("human", "{query}"),
-        ("placeholder", "{agent_scratchpad}"),
-    ]
-).partial(format_instructions=parser.get_format_instructions())
-
-tools = [search_tool, wiki_tool, save_tool]
-agent = create_tool_calling_agent(
-    llm=llm,
-    prompt=prompt,
-    tools=tools
-)
-
-agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-query = input("What can I help you with?")
-raw_response = agent_executor.invoke({"query": query})
-print(raw_response)
-
-try:
-    output_text = raw_response.get("output", [{}])[0].get("text", "")
-    structured_response = parser.parse(output_text)
-    print(structured_response)
-    print(structured_response.topic)
-except Exception as e:
-    print("Error parsing response:", e, "Raw Response - ", raw_response)
+if __name__ == "__main__":
+    main()
