2e42b7e4b4277142f3b7cc4b9bb448bb8a65517f
arcor3 <arcor3@morgan.edu>
2026-04-16 12:55:56 -0400

    Remove sub-agents from root_agent
    Removed labor and economic sub-agents from root_agent.

--- Presidential AI Agent/my_agent/agent.py
+++ Presidential AI Agent/my_agent/agent.py
@@ -1,8 +1,7 @@
 from google.adk.agents.llm_agent import Agent
 from google.adk.tools.google_search_tool import GoogleSearchTool
 from .prompt import ROOT_AGENT_INSTR
-from sub_agents.labor_agent import labor_agent
-from sub_agents.economic_agent import economic_agent
+
 
 def get_current_time(city: str) -> dict:
     return {"status": "success", "city": city, "time": "10:30 AM"}
@@ -13,6 +12,5 @@ root_agent = Agent(
     description='A helpful assistant for Political Leadership questions.',
     instruction=ROOT_AGENT_INSTR,
     tools=[GoogleSearchTool()],
-    sub_agents=[ labor_agent,
-    economic_agent],
+    sub_agents=[ ],
 )
