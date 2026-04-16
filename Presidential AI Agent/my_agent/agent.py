3cc729e0c5657cb7794f9f96beb342879fd93ff3
FanVtic <116291839+FanVtic@users.noreply.github.com>
2026-04-15 22:25:23 -0400

    Add files via upload

---
+++ Presidential AI Agent/my_agent/agent.py
@@ -0,0 +1,15 @@
+from google.adk.agents.llm_agent import Agent
+from google.adk.tools.google_search_tool import GoogleSearchTool
+from .prompt import ROOT_AGENT_INSTR
+
+def get_current_time(city: str) -> dict:
+    return {"status": "success", "city": city, "time": "10:30 AM"}
+
+root_agent = Agent(
+    model='gemini-2.5-flash',
+    name='root_agent',
+    description='A helpful assistant for Political Leadership questions.',
+    instruction=ROOT_AGENT_INSTR,
+    tools=[GoogleSearchTool()],
+    sub_agents=[],
+)
